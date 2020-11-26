02a180847da9223d7e835f331347e7597ee7a30f
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2020-11-25 21:01:10 -0300

    add aula de rewarded ads

--- app/src/main/java/com/example/notificacao_admob/MainActivity.java
+++ app/src/main/java/com/example/notificacao_admob/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.notificacao_admob;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
@@ -12,32 +13,40 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdCallback;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnNotificacao, btnPublicidade, btnVideo;
     AdView adView;
     InterstitialAd interstitialAd;
+    RewardedAd rewardedAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         adView = findViewById(R.id.adView);
-        AdRequest request = new AdRequest.Builder().build();
-        adView.loadAd(request);
-        criarPublicidade();
-        criarPubVideo();
         btnPublicidade = findViewById(R.id.btnPublicidade);
         btnVideo = findViewById(R.id.btnVideo);
         btnNotificacao = findViewById(R.id.btnNotificacao);
+        AdRequest request = new AdRequest.Builder().build();
+        adView.loadAd(request);
+        criarPubVideo();
+        criarPublicidade();
 
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
@@ -60,7 +69,49 @@ public class MainActivity extends AppCompatActivity {
 
         btnVideo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) { }
+            public void onClick(View view) {
+                RewardedAdCallback adCallback = new RewardedAdCallback() {
+                    @Override
+                    public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
+                        Toast.makeText(
+                                MainActivity.this,
+                                "Viu ate o final. Ganhou o premio",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    }
+
+                    @Override
+                    public void onRewardedAdOpened() {
+                        super.onRewardedAdOpened();
+                        Toast.makeText(
+                                MainActivity.this,
+                                "Video foi aberto",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    }
+
+                    @Override
+                    public void onRewardedAdClosed() {
+                        super.onRewardedAdClosed();
+                        Toast.makeText(
+                                MainActivity.this,
+                                "Video foi fechado",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    }
+
+                    @Override
+                    public void onRewardedAdFailedToShow(AdError adError) {
+                        super.onRewardedAdFailedToShow(adError);
+                        Toast.makeText(
+                                MainActivity.this,
+                                "Falhou ao abrir",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                };
+                rewardedAd.show(MainActivity.this, adCallback);
+            }
         });
     }
 
@@ -70,7 +121,31 @@ public class MainActivity extends AppCompatActivity {
         interstitialAd.loadAd(new AdRequest.Builder().build());
     }
 
-    private void criarPubVideo() {}
+    private void criarPubVideo() {
+        rewardedAd = new RewardedAd(this, "ca-app-pub-3940256099942544/5224354917");
+        RewardedAdLoadCallback adLoadCallback = new RewardedAdLoadCallback() {
+            @Override
+            public void onRewardedAdLoaded() {
+                super.onRewardedAdLoaded();
+                Toast.makeText(
+                        MainActivity.this,
+                        "Video carregado com sucesso.",
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+
+            @Override
+            public void onRewardedAdFailedToLoad(LoadAdError loadAdError) {
+                super.onRewardedAdFailedToLoad(loadAdError);
+                Toast.makeText(
+                        MainActivity.this,
+                        "Erro ao carregar o video",
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        };
+        rewardedAd.loadAd(new AdRequest.Builder().build(), adLoadCallback);
+    }
 
     private void criarNotificacao(String texto) {
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
